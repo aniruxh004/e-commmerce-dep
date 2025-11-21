d610721970c99ee8220e0af7600bc39aa273eecf
aniruxh004 <131795735+aniruxh004@users.noreply.github.com>
2025-11-21 23:11:42 +0530

    Update CheckoutPage.js

--- frontend/src/components/CheckoutPage.js
+++ frontend/src/components/CheckoutPage.js
@@ -29,7 +29,7 @@ export default function CheckoutPage() {
 
     try {
       const token = localStorage.getItem('token');
-      const res = await fetch('http://localhost:5000/api/orders/create-order', {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/orders/create-order`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
